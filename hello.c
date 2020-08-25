56ee683ee71a543be4037be9d36d6ab04c3ab68e
Devyn Boehm <dboehm2@huskers.unl.edu>
2020-08-25 01:17:23 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Devyn Boehm
+ *         dboehm2@huskers.unl.edu
+ * Date: 2020/08/18
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Devyn Boehm\n");
+
+  return 0;
+}
